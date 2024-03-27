7259df6c90f800ac72d5c87a744bb847cab3e7f1
deguree2718 <85945180+deguree2718@users.noreply.github.com>
2024-03-27 17:29:50 -0300

    Delete .github/workflows/main.sh

--- .github/workflows/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt install curl
-
-curl -x POST https://discord.com/api/webhooks/1222639782115545189/nD00Hvi2Gy0SrM1L3uW4dVnQlDweJurTWZmknqd19Lcgba6tdHGg_Svdh7Fsy3Fm63xf --json '{ "content": "TEST"}'
