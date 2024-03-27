c25dcca5ee562270a684bae86cdcc8b033d04448
deguree2718 <deguree2.718@gmail.com>
2024-03-27 17:24:48 -0300

    updated main for testing

--- src/main/java/com/deod/Main.java
+++ src/main/java/com/deod/Main.java
@@ -3,15 +3,21 @@ package com.deod;
 import org.javacord.api.*;
 import org.javacord.api.entity.emoji.CustomEmojiBuilder;
 import org.javacord.api.entity.intent.*;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.sticker.StickerBuilder;
+import org.javacord.api.entity.sticker.StickerFormatType;
+import org.javacord.api.entity.sticker.StickerItem;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.interaction.*;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
@@ -32,13 +38,13 @@ public class Main {
                             .setDescription("Guiding you thorugh Deod")
                             .setAuthor("ya boy Degure")
                             .addField("$help", "Show this embed, listing the possible commands for Deod")
-                            .addInlineField("$downloadCreate :customEmote:", "sends the source of the emote, and adds it into the current server (doesn't work on DMs)")
-                            .addInlineField("$download :customEmote:", "sends the source of the emote (does work on DMs)")
-                            .addInlineField("$propertyIsTheft :customEmote:", "adds the emote into the current server (doesn't work on DMs)")
-                            .addInlineField("$roll [number]", "generates a RNG between 1 and the number sent, standard is 20, so if you don't send any numbers it'll roll between 1 and 20")
+                            .addField("$downloadCreate :customEmote:", "sends the source of the emote, and adds it into the current server (doesn't work on DMs)")
+                            .addField("$download :customEmote:", "sends the source of the emote (does work on DMs)")
+                            .addField("$propertyIsTheft :customEmote:", "adds the emote into the current server (doesn't work on DMs)")
+                            .addField("$roll [number]", "generates a RNG between 1 and the number sent, standard is 20, so if you don't send any numbers it'll roll between 1 and 20")
+                            .addField("$nitro :customEmote:", "Sends the source image for the emote, whilst deleting the trigger message")
                             .setColor(Color.CYAN);
                     event.getChannel().sendMessage(embed);
-                    log(event);
                 }
                 else if (event.getMessageContent().startsWith("$downloadCreate")) {
                     if (event.getMessageContent().contains("<:") || event.getMessageContent().contains("<a:")) {
@@ -57,7 +63,6 @@ public class Main {
                             emojiBuilder.setImage(imageUrl);
                             emojiBuilder.create();
                         }
-                        log(event);
                     }
                 } else if (event.getMessageContent().startsWith("$propertyIsTheft")){
                     if (event.getMessageContent().contains("<:") || event.getMessageContent().contains("<a:")) {
@@ -75,7 +80,6 @@ public class Main {
                             emojiBuilder.setImage(imageUrl);
                             emojiBuilder.create();
                         }
-                        log(event);
                     }
                 } else if (event.getMessageContent().startsWith("$download")){
                     if (event.getMessageContent().contains("<:") || event.getMessageContent().contains("<a:")){
@@ -86,19 +90,42 @@ public class Main {
                         String url = "https://cdn.discordapp.com/emojis/" + idEmote + (isAnimated ? ".gif" : ".png");
                         event.getChannel().sendMessage(url);
                     }
-                    log(event);
                 } else if (event.getMessageContent().startsWith("$roll")){
                     String amount = event.getMessageContent().equals("$roll") || event.getMessageContent().equals("!roll ") ? "20" : event.getMessageContent().substring(6);
                     Random rng = new Random();
                     Integer generated = rng.ints(1, 1, Integer.parseInt(amount)).sum();
                     event.getChannel().sendMessage(generated.toString());
                     log(event);
-                } else if (event.getMessageContent().equals("$my_github")){
+                } else if (event.getMessageContent().equals("$my_github")) {
                     event.getChannel().sendMessage("https://github.com/deguree2718/deod");
+                } else if (event.getMessageContent().startsWith("$nitro")){
+                    String emote = event.getMessageContent().substring(event.getMessageContent().indexOf("<"), event.getMessageContent().indexOf(">"));
+                    List<String> splited = List.of(emote.split(":"));
+                    boolean isAnimated = splited.get(0).equals("<a");
+                    String idEmote = splited.get(splited.size() - 1).replace(">", "");
+                    String url = "https://cdn.discordapp.com/emojis/" + idEmote + (isAnimated ? ".gif" : ".png");
+                    event.getChannel().sendMessage(url);
+                    var delete = event.getMessage().canYouDelete() ? event.getMessage().delete() : null;
+                }
+                if (event.getMessageContent().startsWith("$")){
                     log(event);
                 }
             } catch (Exception e){
                 log(e, event);
+            }
+        });
+
+        api.addReactionAddListener(event -> {
+            try{
+                if (event.getEmoji().asUnicodeEmoji().get().equals("U+1F4BE")){
+                    long messageId = event.getMessageId();
+                    Message message = api.getMessageById(messageId, event.getChannel()).get();
+                    StickerItem sticker = message.getStickerItems().stream().findFirst().get();
+                    URI image = new URI("https://media.discordapp.net/stickers/" + sticker.getIdAsString() + ".png?size=480");
+                    message.getChannel().sendMessage(image.toString());
+                }
+            } catch (Exception e){
+               System.out.println(e.getMessage());
             }
         });
     }
