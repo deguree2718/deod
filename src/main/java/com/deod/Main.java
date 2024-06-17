613721ca7fa1ef2fc9dc1d46d8e30303e20d926b
deguree2718 <deguree2.718@gmail.com>
2024-06-17 11:26:09 -0300

    added some rules and flags to the \ command, and updated the \ embed

--- src/main/java/com/deod/Main.java
+++ src/main/java/com/deod/Main.java
@@ -22,6 +22,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -36,13 +37,18 @@ public class Main {
                     EmbedBuilder embed = new EmbedBuilder()
                             .setTitle("Help")
                             .setDescription("Guiding you through Deod")
-                            .setAuthor("ya boy Degure")
+                            .setAuthor("degu, the medieval coder")
                             .addField("$help", "Show this embed, listing the possible commands for Deod")
                             .addField("$downloadCreate :customEmote:", "sends the source of the emote, and adds it into the current server (doesn't work on DMs)")
                             .addField("$download :customEmote:", "sends the source of the emote (does work on DMs)")
                             .addField("$propertyIsTheft :customEmote:", "adds the emote into the current server (doesn't work on DMs)")
                             .addField("$roll [number]", "generates a RNG between 1 and the number sent, standard is 20, so if you don't send any numbers it'll roll between 1 and 20")
+                            .addField("$roll [number]d[number]", "generates an amount of numbers equal to the first number, each with a max equal to the second number, shows the numbers rolled and the sum of them all")
+                            .addInlineField("adv", "with $roll you can send also the adv flag, which will show all numbers rolled, but it will not sum them all, instead returning the highest rolled value")
+                            .addInlineField("disadv", "with $roll you can send also the disadv flag, which will show all numbers rolled, but it will not sum them all, instead returning the lowest rolled value")
+                            .addInlineField("h3", "with $roll you can send also the h3 flag, which will show all numbers rolled, but it will sum only the 3 highest values")
                             .addField("$nitro :customEmote:", "Sends the source image for the emote, whilst deleting the trigger message")
+                            .addField("$my_github", "Nothing important really, it's just the bot's github page, it has the permalink invite for the bot and an out of date README (I will update it, one day)")
                             .setColor(Color.CYAN);
                     event.getChannel().sendMessage(embed);
                 }
@@ -91,10 +97,46 @@ public class Main {
                         event.getChannel().sendMessage(url);
                     }
                 } else if (event.getMessageContent().startsWith("$roll")){
-                    String amount = event.getMessageContent().equals("$roll") || event.getMessageContent().equals("!roll ") ? "20" : event.getMessageContent().substring(6);
+                    String amount = event.getMessageContent().equals("$roll") ? "20" : event.getMessageContent().substring(6);
+                    String flags = "";
+                    if(amount.indexOf(" ") > -1){
+                        flags = amount.substring(amount.indexOf(" "));
+                        amount = amount.substring(0, amount.indexOf(" "));
+                    }
                     Random rng = new Random();
-                    Integer generated = rng.ints(1, 1, Integer.parseInt(amount)).sum();
-                    event.getChannel().sendMessage(generated.toString());
+                    Integer generated = 0;
+                    if (amount.contains("d")){
+                        Integer quantity = Integer.parseInt(amount.split("d")[0]);
+                        Integer sides = Integer.parseInt(amount.split("d")[1]);
+                        List<Integer> generatedList = rng.ints(quantity,1,sides).boxed().collect(Collectors.toList());
+                        if (flags.contains("disadv")){
+                            for(Integer val : generatedList){
+                                if (val < generated || generated == 0) {
+                                    generated = val;
+                                }
+                            }
+                        } else if (flags.contains("adv")){
+                            for(Integer val : generatedList){
+                                if (val > generated) {
+                                    generated = val;
+                                }
+                            }
+                        } else if (flags.contains("h3") || quantity >= 3) {
+                            generatedList.sort(null);
+                            Collections.reverse(generatedList);
+                            generated = generatedList.get(0) + generatedList.get(1) + generatedList.get(2);
+                        }
+                        else {
+                            for(Integer val : generatedList){
+                                generated += val;
+                            }
+                        }
+                        event.getChannel().sendMessage(generatedList.toString() + "\n" +
+                                "**" + generated + "**");
+                    } else {
+                        generated = rng.ints(1, 1, Integer.parseInt(amount)).sum();
+                        event.getChannel().sendMessage(generated.toString());
+                    }
                 } else if (event.getMessageContent().equals("$my_github")) {
                     event.getChannel().sendMessage("https://github.com/deguree2718/deod");
                 } else if (event.getMessageContent().startsWith("$nitro")){
